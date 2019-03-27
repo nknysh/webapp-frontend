c6e0f32f0e3836472746a03449be80f25cf24631
Kris Pomphrey <kris@tkf.network>
2019-03-27 09:07:13 +0000

    fix tests

--- src/store/modules/booking/reducer.test.js
+++ src/store/modules/booking/reducer.test.js
@@ -3,12 +3,12 @@ import { initialState } from 'store/common';
 import { updateBooking } from './actions';
 import reducer from './reducer';
 
-describe('countries reducer', () => {
+describe('bookings reducer', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(initialState);
   });
 
-  it('should handle SET_COUNTRIES', () => {
+  it('should handle BOOKING_UPDATE', () => {
     const action = updateBooking();
     const reducedState = reducer({}, action);
 
