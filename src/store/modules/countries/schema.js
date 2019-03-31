238c4021565d8bf8412cd816f38d1f1709c9cbf6
Kris Pomphrey <kris@tkf.network>
2019-03-31 22:16:21 +0100

    Revert countries id

--- src/store/modules/countries/schema.js
+++ src/store/modules/countries/schema.js
@@ -7,5 +7,5 @@ const id = 'code';
 export default {
   id,
   index: ['name'],
-  schema: new schema.Entity('countries', {}, schemaOpts),
+  schema: new schema.Entity('countries', {}, { idAttribute: id }),
 };
