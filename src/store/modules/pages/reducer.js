dd05d98b4c93da65476995d1ac20bfb23494b2d6
Kris Pomphrey <kris@tkf.network>
2019-04-24 07:26:59 +0100

    Update page redux key

--- src/store/modules/pages/reducer.js
+++ src/store/modules/pages/reducer.js
@@ -1,13 +1,13 @@
-import { createReducer, getErrorActionName, getSuccessActionName } from 'store/utils';
+import { createReducer, getErrorActionName, getSuccessActionName, getLoadingActionName } from 'store/utils';
 import { initialState, loadingReducer, successReducer, errorReducer } from 'store/common';
 
-import { FETCH_PAGE } from './actions';
+import { PAGE } from './actions';
 
 export default createReducer(
   {
-    [FETCH_PAGE]: loadingReducer,
-    [getSuccessActionName(FETCH_PAGE)]: successReducer,
-    [getErrorActionName(FETCH_PAGE)]: errorReducer,
+    [getLoadingActionName(PAGE)]: loadingReducer,
+    [getSuccessActionName(PAGE)]: successReducer,
+    [getErrorActionName(PAGE)]: errorReducer,
   },
   initialState
 );
