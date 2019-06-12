262cc0ef7a834897b6db81f09e1e859c59440cb3
Kris Pomphrey <kris@tkf.network>
2019-06-12 15:46:29 +0100

    Default admin api var

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
-import { equals } from 'ramda';
+import { defaultTo, equals } from 'ramda';
 
 export const APP_ENV = process.env.NODE_ENV;
 export const APP_VERBOSE_ERRORS = equals('true', process.env.APP_VERBOSE_ERRORS);
 
-export const API_BASE_URL = process.env.API_BASE_URL;
+export const API_BASE_URL = defaultTo('/api', process.env.API_BASE_URL);
 export const ADMIN_BASE_URL = process.env.ADMIN_BASE_URL;
 
 export const PAYMENT_ENABLED = equals('true', process.env.PAYMENT_ENABLED);
