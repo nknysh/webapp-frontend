55befc598706ead8749f3276af46a05d5e0d8f36
Kris Pomphrey <kris@tkf.network>
2019-07-17 18:47:03 +0100

    OWA-441 - Move to next tab on scroll button click

--- src/components/elements/Tabs/Tabs.jsx
+++ src/components/elements/Tabs/Tabs.jsx
@@ -1,5 +1,6 @@
-import React, { useState, Fragment } from 'react';
-import { propOr, isEmpty, partial } from 'ramda';
+import React, { useState, Fragment, useCallback, Children } from 'react';
+import { propOr, isEmpty, partial, inc, gt, length, lt, dec, equals } from 'ramda';
+import TabScrollButton from '@material-ui/core/Tabs/TabScrollButton';
 
 import { mapWithIndex } from 'utils';
 
@@ -8,22 +9,63 @@ import { StyledTabs, Tab } from './Tabs.styles';
 
 const renderLabel = ({ tabClassname }, label, i) => <Tab key={i} className={tabClassname} label={label} />;
 
-export const Tabs = ({ labels, children, onChange, value, tabClassname, ...props }) => {
+const renderTabArrow = ({ onTabScrollChange }) => {
+  return props => {
+    const { direction, onClick } = props;
+
+    const onClickHandler = (...args) => {
+      onTabScrollChange(direction);
+      onClick(...args);
+    };
+
+    return <TabScrollButton {...props} onClick={onClickHandler} />;
+  };
+};
+
+export const Tabs = ({ labels, children, onChange, value, tabClassname, onTabScrollButtonChange, ...props }) => {
   const [tab, setTab] = useState(value);
 
-  const content = propOr(children, tab, children);
-  const onTabSelect = (e, value) => {
-    setTab(value);
-    onChange(value);
-  };
+  const childrenArr = Children.toArray(children);
+  const childrenLength = length(childrenArr);
+
+  const content = propOr(childrenArr, tab, childrenArr);
+
+  const onTabScrollChange = useCallback(
+    direction => {
+      onTabScrollButtonChange(direction);
+      const next = inc(tab);
+      const prev = dec(tab);
+
+      const nextTab = equals('right', direction) ? (gt(next, childrenLength) ? tab : next) : lt(prev, 0) ? tab : prev;
+
+      setTab(nextTab);
+    },
+    [tab, childrenArr, childrenLength]
+  );
+
+  const onTabSelect = useCallback(
+    (e, value) => {
+      setTab(value);
+      onChange(value);
+    },
+    [onChange]
+  );
 
   return (
     <Fragment>
       {!isEmpty(labels) && (
-        <StyledTabs classes={{ indicator: 'tab-line' }} value={tab} className="tabs" onChange={onTabSelect} {...props}>
+        <StyledTabs
+          ScrollButtonComponent={renderTabArrow({ onTabScrollChange })}
+          classes={{ indicator: 'tab-line' }}
+          value={tab}
+          className="tabs"
+          onChange={onTabSelect}
+          {...props}
+        >
           {mapWithIndex(partial(renderLabel, [{ tabClassname }]), labels)}
         </StyledTabs>
       )}
+
       {content}
     </Fragment>
   );
