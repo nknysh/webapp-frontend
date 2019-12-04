d94d66f78f83e12250730784d55f26209195df18
Tom Gray <tom@pure-escapes.com>
2019-12-04 16:00:06 +0000

    restore

--- src/containers/BookingContainer/BookingContainer.state.js
+++ src/containers/BookingContainer/BookingContainer.state.js
@@ -15,7 +15,6 @@ import {
   getBookingCreatedByValue,
   getBookingCreated,
   getBookingHolds,
-  getBookingCurrencySymbol,
 } from 'store/modules/bookings';
 
 import { forwardsCompatBookingBuilderAction } from 'store/modules/bookingBuilder';
@@ -25,8 +24,6 @@ export const mapStateToProps = (state, { id }) => ({
   holds: getBookingHolds(state, id),
   bookingStatus: getBookingStatus(state),
   created: getBookingCreatedByValue(state, id),
-  currencyCode: getBookingCurrencySymbol(state, id),
-
   amended: getBookingCreated(state, id),
 });
 
