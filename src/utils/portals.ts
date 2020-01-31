40239f83e428ff9c61cc21334ed2d269b3052880
Tom Elders <him@tomelders.com>
2020-01-31 09:26:29 +0000

    [OWA-1088] Add Z-Index to Portal containers

--- src/utils/portals.ts
+++ src/utils/portals.ts
@@ -2,19 +2,30 @@ import ReactDOM from 'react-dom';
 import { ReactPortal } from 'react';
 
 export enum PortalType {
-  Tooltip = 'portal-tooltips',
-  Modal = 'portal-modals',
+  Tooltip = 'portal-tooltip',
+  Modal = 'portal-modal',
+  Overlay = 'portal-overlay',
 }
 
 // Use this to guarantee the order of portal divs to
 // ensure tooltips are always above modals, etc etc
-const portalOrder = [PortalType.Modal, PortalType.Tooltip];
+const portalOrder = [PortalType.Modal, PortalType.Tooltip, PortalType.Overlay];
 
 const appendPortalElementsWithOrder = (orderedTypes: PortalType[]) => () => {
+  // Thanks to the liberal application of z-indexes without
+  // any comprehension of how the work, or any thought to
+  // the problems they cause, we need to set a z-index for each
+  // modal container. How did I arrive at this number? well, everytime
+  // I discover an element with some stupidly high z-index, I have to
+  //  increment this number.
+
+  const zIndex = 1300;
+
   const fragment = document.createDocumentFragment();
-  orderedTypes.forEach(id => {
-    const el = document.createElement('div');
+  orderedTypes.forEach((id, idx) => {
+    const el = document.createElement('span');
     el.setAttribute('id', id);
+    el.setAttribute('style', `z-index:${zIndex + idx}; width: 0px;`);
     fragment.appendChild(el);
   });
   document.body.appendChild(fragment);
