6c28794237e8fa432d03a643fad625d8b3e2eccb
Tom Elders <him@tomelders.com>
2020-03-23 17:44:07 +0000

    [OWA-1370] Country Select Fix

--- src/containers/UserPanel/UserPanel.styles.js
+++ src/containers/UserPanel/UserPanel.styles.js
@@ -1,7 +1,20 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import { Select, Link as BaseLink } from '@pure-escapes/webapp-ui-components';
 
 import { theme } from 'styles';
+import { pureUiTheme } from 'pureUi/pureUiTheme';
+
+const dropdownIn = keyframes`
+  from {
+    opacity: 0;
+    top: 15px;
+  }
+
+  to {
+    opacity: 1;
+    top: 5px;
+  }
+`;
 
 export const StyledUserPanel = styled.div`
   position: relative;
@@ -10,27 +23,69 @@ export const StyledUserPanel = styled.div`
   ${props => props.theme.breakpoints.tablet`
     padding: 0;
   `}
+
+  .link {
+    color: ${pureUiTheme.colors.black};
+    font-size: 16px;
+    display: block;
+  }
+
+  ul.dropdown {
+    animation: ${dropdownIn} 0.25s ease-out;
+    border-radius: 8px;
+    position: absolute;
+    right: 0;
+    top: 5px;
+    background-color: white;
+    padding: 10px 0;
+    list-style: none;
+    margin: 0;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    border: ${pureUiTheme.colorRoles.lightGreyBorder} 1px solid;
+    color: ${pureUiTheme.colors.black};
+    font: ${pureUiTheme.typography.sansSerifFont};
+    text-transform: uppercase;
+  }
+
+  ul.dropdown > li {
+    font-size: 16px;
+    padding: 10px;
+    border-bottom: ${pureUiTheme.colorRoles.lightGreyBorder} 1px solid;
+
+    &:last-child {
+      border: none;
+    }
+  }
+
+  .dropdownBG {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0);
+  }
 `;
 
 export const Text = styled.div`
-    margin ${theme.spacing.gutter / 2}px 0;
-    padding: ${theme.spacing.gutter / 2}px 0;
-    text-transform: uppercase;
-    font-weight: ${theme.fonts.bold};
-    cursor: pointer;
-    transition: ${theme.defaultTransition};
+  margin: ${theme.spacing.gutter / 2}px 0;
+  padding: ${theme.spacing.gutter / 2}px 0;
+  text-transform: uppercase;
+  font-weight: ${theme.fonts.bold};
+  cursor: pointer;
+  transition: ${theme.defaultTransition};
 
-    &:hover {
-        color: ${theme.palette.primary};
-    }
+  &:hover {
+    color: ${theme.palette.primary};
+  }
 
-    ${({ ['data-placeholder']: placeholder }) =>
-      placeholder &&
-      css`
-        color: ${theme.colors.gray};
-      `}
+  ${({ ['data-placeholder']: placeholder }) =>
+    placeholder &&
+    css`
+      color: ${theme.colors.gray};
+    `}
 
-    ${props => props.theme.breakpoints.tablet`
+  ${props => props.theme.breakpoints.tablet`
       margin-left: ${theme.spacing.gutter}px;
       padding-left: ${theme.spacing.gutter}px;
     `}
