6564a9fbc11d28b00ed01085263452e139181aea
Tom Elders <him@tomelders.com>
2020-03-27 09:02:53 +0000

    [HOT FIX] COmment out seelctor with dodgy import

--- src/store/modules/offer/subdomains/uiState/selectors.ts
+++ src/store/modules/offer/subdomains/uiState/selectors.ts
@@ -7,18 +7,30 @@ import { getOffersOnHotelSelector } from '../../selectors';
 // in webpack or babel?
 const tmpOfferDomainSelector = (state: any): IOfferModel => state.offer;
 
-export const uiStateSelector = createSelector(tmpOfferDomainSelector, domain => domain.uiState);
+export const uiStateSelector = createSelector(
+  tmpOfferDomainSelector,
+  domain => domain.uiState
+);
 
 export const getOfferRequestIsPendingSelector = createSelector(
   uiStateSelector,
   uiState => uiState.getOfferRequestIsPending
 );
 
-export const getOfferErrorSelector = createSelector(uiStateSelector, uiState => uiState.getError);
+export const getOfferErrorSelector = createSelector(
+  uiStateSelector,
+  uiState => uiState.getError
+);
 
-export const putOfferErrorSelector = createSelector(uiStateSelector, uiState => uiState.putError);
+export const putOfferErrorSelector = createSelector(
+  uiStateSelector,
+  uiState => uiState.putError
+);
 
-export const postOfferErrorSelector = createSelector(uiStateSelector, uiState => uiState.postError);
+export const postOfferErrorSelector = createSelector(
+  uiStateSelector,
+  uiState => uiState.postError
+);
 
 export const offerDomainIsTextOnlySelector = createSelector(
   uiStateSelector,
@@ -40,24 +52,24 @@ export const combinationOfferUuidsSelector = createSelector(
   uiStateSelector => uiStateSelector.combinationOfferUuids
 );
 
-export const combinationListSelector = createSelector(
-  combinationOfferUuidsSelector,
-  getOffersOnHotelSelector,
-  (combinationList, offersOnHotel) => {
-    return offersOnHotel.map(offerOnHotel => {
-      if (combinationList.includes(offerOnHotel.uuid)) {
-        return {
-          uuid: offerOnHotel.uuid,
-          label: offerOnHotel.name,
-          value: true,
-        };
-      } else {
-        return {
-          uuid: offerOnHotel.uuid,
-          label: offerOnHotel.name,
-          value: false,
-        };
-      }
-    });
-  }
-);
+// export const combinationListSelector = createSelector(
+//   combinationOfferUuidsSelector,
+//   getOffersOnHotelSelector,
+//   (combinationList, offersOnHotel) => {
+//     return offersOnHotel.map(offerOnHotel => {
+//       if (combinationList.includes(offerOnHotel.uuid)) {
+//         return {
+//           uuid: offerOnHotel.uuid,
+//           label: offerOnHotel.name,
+//           value: true,
+//         };
+//       } else {
+//         return {
+//           uuid: offerOnHotel.uuid,
+//           label: offerOnHotel.name,
+//           value: false,
+//         };
+//       }
+//     });
+//   }
+// );
