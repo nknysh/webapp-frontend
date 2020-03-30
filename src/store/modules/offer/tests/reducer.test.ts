f391bef0200e6b40cdf0f2f2f2a08ad661c7ae9b
Tom Gray <tom@pure-escapes.com>
2020-03-30 09:31:48 +0100

    removed line break

--- src/store/modules/offer/tests/reducer.test.ts
+++ src/store/modules/offer/tests/reducer.test.ts
@@ -65,7 +65,6 @@ describe('accommodationProductsForHotelReducer', () => {
     const result = reducer(undefined, action);
     const expected: IOfferModel = {
       ...initialState,
-
       accommodationProductsForHotel: fixture,
     };
     expect(result).toEqual(expected);
