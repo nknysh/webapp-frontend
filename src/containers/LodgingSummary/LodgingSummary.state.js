fda074cde97ca2eee46d098c1ffc815a445ff31d
andrew wilde <andrew.wilde@jsgeeks.com>
2020-03-31 17:35:29 +0600

    prettier

--- src/containers/LodgingSummary/LodgingSummary.state.js
+++ src/containers/LodgingSummary/LodgingSummary.state.js
@@ -33,4 +33,7 @@ const mapDispatchToProps = dispatch =>
     dispatch
   );
 
-export default connect(mapStateToProps, mapDispatchToProps);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
