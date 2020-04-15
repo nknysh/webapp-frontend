1eea38b389c622ea7ab3a3ce43ec4cb7af3a2400
Tom Gray <tom@pure-escapes.com>
2020-04-15 10:39:51 +0100

    tests for offers list query selector

--- src/store/modules/offersList/selectors.test.ts
+++ src/store/modules/offersList/selectors.test.ts
@@ -1,5 +1,5 @@
 import { IOffersListDomain, initialState } from './model';
-import { selectedHotelSelector, pageCountSelector } from './selectors';
+import { selectedHotelSelector, pageCountSelector, offersListQuerySelector } from './selectors';
 
 describe('offersList selector', () => {
   it('selectedHotelSelector initial state (empty string)', () => {
@@ -45,6 +45,241 @@ describe('offersList selector', () => {
       const totalResultsFixture = 11;
 
       expect(pageCountSelector.resultFunc(itemsPerPageFixture, totalResultsFixture)).toEqual(6);
+    });
+  });
+
+  describe('offersListQuerySelector', () => {
+    it('with defaults', () => {
+      const sortByFixture = 'createdAt';
+      const filterFixture = '';
+      const currentPageFixture = 0;
+      const itemsPerPageFixture = 10;
+      const sortOrderFixture = 'asc';
+      const filterFieldsFixture = ['uuid', 'name'];
+      const selectedHotelFixture = '';
+
+      const result = offersListQuerySelector.resultFunc(
+        sortByFixture,
+        filterFixture,
+        currentPageFixture,
+        itemsPerPageFixture,
+        sortOrderFixture,
+        filterFieldsFixture,
+        selectedHotelFixture
+      );
+      expect(result).toMatchObject({
+        sort: 'offer.createdAt',
+        page: { offset: 0, limit: 10 },
+        associations: 'hotel',
+        fields: { hotel: 'uuid, name, countryCode' },
+        filter: { offer: {} },
+      });
+    });
+
+    it('different sort by', () => {
+      const sortByFixture = 'name';
+      const filterFixture = '';
+      const currentPageFixture = 0;
+      const itemsPerPageFixture = 10;
+      const sortOrderFixture = 'asc';
+      const filterFieldsFixture = ['uuid', 'name'];
+      const selectedHotelFixture = '';
+
+      const result = offersListQuerySelector.resultFunc(
+        sortByFixture,
+        filterFixture,
+        currentPageFixture,
+        itemsPerPageFixture,
+        sortOrderFixture,
+        filterFieldsFixture,
+        selectedHotelFixture
+      );
+      expect(result).toMatchObject({
+        sort: 'offer.name',
+        page: { offset: 0, limit: 10 },
+        associations: 'hotel',
+        fields: { hotel: 'uuid, name, countryCode' },
+        filter: { offer: {} },
+      });
+    });
+
+    it('sort order desc', () => {
+      const sortByFixture = 'createdAt';
+      const filterFixture = '';
+      const currentPageFixture = 0;
+      const itemsPerPageFixture = 10;
+      const sortOrderFixture = 'desc';
+      const filterFieldsFixture = ['uuid', 'name'];
+      const selectedHotelFixture = '';
+
+      const result = offersListQuerySelector.resultFunc(
+        sortByFixture,
+        filterFixture,
+        currentPageFixture,
+        itemsPerPageFixture,
+        sortOrderFixture,
+        filterFieldsFixture,
+        selectedHotelFixture
+      );
+      expect(result).toMatchObject({
+        sort: '-offer.createdAt',
+        page: { offset: 0, limit: 10 },
+        associations: 'hotel',
+        fields: { hotel: 'uuid, name, countryCode' },
+        filter: { offer: {} },
+      });
+    });
+
+    it('with a filter', () => {
+      const sortByFixture = 'createdAt';
+      const filterFixture = 'TEST';
+      const currentPageFixture = 0;
+      const itemsPerPageFixture = 10;
+      const sortOrderFixture = 'asc';
+      const filterFieldsFixture = ['uuid', 'name'];
+      const selectedHotelFixture = '';
+
+      const result = offersListQuerySelector.resultFunc(
+        sortByFixture,
+        filterFixture,
+        currentPageFixture,
+        itemsPerPageFixture,
+        sortOrderFixture,
+        filterFieldsFixture,
+        selectedHotelFixture
+      );
+      expect(result).toMatchObject({
+        sort: 'offer.createdAt',
+        page: { offset: 0, limit: 10 },
+        associations: 'hotel',
+        fields: { hotel: 'uuid, name, countryCode' },
+        filter: {
+          offer: {
+            'uuid,name:ilike': 'TEST',
+          },
+        },
+      });
+    });
+
+    it('with a selected hotel', () => {
+      const sortByFixture = 'createdAt';
+      const filterFixture = '';
+      const currentPageFixture = 0;
+      const itemsPerPageFixture = 10;
+      const sortOrderFixture = 'asc';
+      const filterFieldsFixture = ['uuid', 'name'];
+      const selectedHotelFixture = 'A';
+
+      const result = offersListQuerySelector.resultFunc(
+        sortByFixture,
+        filterFixture,
+        currentPageFixture,
+        itemsPerPageFixture,
+        sortOrderFixture,
+        filterFieldsFixture,
+        selectedHotelFixture
+      );
+      expect(result).toMatchObject({
+        sort: 'offer.createdAt',
+        page: { offset: 0, limit: 10 },
+        associations: 'hotel',
+        fields: { hotel: 'uuid, name, countryCode' },
+        filter: {
+          offer: {
+            hotelUuid: 'A',
+          },
+        },
+      });
+    });
+
+    it('with a filter and a selected hotel', () => {
+      const sortByFixture = 'createdAt';
+      const filterFixture = 'TEST';
+      const currentPageFixture = 0;
+      const itemsPerPageFixture = 10;
+      const sortOrderFixture = 'asc';
+      const filterFieldsFixture = ['uuid', 'name'];
+      const selectedHotelFixture = 'A';
+
+      const result = offersListQuerySelector.resultFunc(
+        sortByFixture,
+        filterFixture,
+        currentPageFixture,
+        itemsPerPageFixture,
+        sortOrderFixture,
+        filterFieldsFixture,
+        selectedHotelFixture
+      );
+      expect(result).toMatchObject({
+        sort: 'offer.createdAt',
+        page: { offset: 0, limit: 10 },
+        associations: 'hotel',
+        fields: { hotel: 'uuid, name, countryCode' },
+        filter: {
+          offer: {
+            'uuid,name:ilike': 'TEST',
+            hotelUuid: 'A',
+          },
+        },
+      });
+    });
+
+    it('higher current page', () => {
+      const sortByFixture = 'createdAt';
+      const filterFixture = '';
+      const currentPageFixture = 3;
+      const itemsPerPageFixture = 10;
+      const sortOrderFixture = 'asc';
+      const filterFieldsFixture = ['uuid', 'name'];
+      const selectedHotelFixture = '';
+
+      const result = offersListQuerySelector.resultFunc(
+        sortByFixture,
+        filterFixture,
+        currentPageFixture,
+        itemsPerPageFixture,
+        sortOrderFixture,
+        filterFieldsFixture,
+        selectedHotelFixture
+      );
+      expect(result).toMatchObject({
+        sort: 'offer.createdAt',
+        page: { offset: 30, limit: 10 },
+        associations: 'hotel',
+        fields: { hotel: 'uuid, name, countryCode' },
+        filter: {
+          offer: {},
+        },
+      });
+    });
+
+    it('higher current page and more items per page', () => {
+      const sortByFixture = 'createdAt';
+      const filterFixture = '';
+      const currentPageFixture = 3;
+      const itemsPerPageFixture = 12;
+      const sortOrderFixture = 'asc';
+      const filterFieldsFixture = ['uuid', 'name'];
+      const selectedHotelFixture = '';
+
+      const result = offersListQuerySelector.resultFunc(
+        sortByFixture,
+        filterFixture,
+        currentPageFixture,
+        itemsPerPageFixture,
+        sortOrderFixture,
+        filterFieldsFixture,
+        selectedHotelFixture
+      );
+      expect(result).toMatchObject({
+        sort: 'offer.createdAt',
+        page: { offset: 36, limit: 12 },
+        associations: 'hotel',
+        fields: { hotel: 'uuid, name, countryCode' },
+        filter: {
+          offer: {},
+        },
+      });
     });
   });
 });
