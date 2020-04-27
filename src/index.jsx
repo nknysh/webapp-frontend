f1ac31e13353508e8a201d3a9f61981da558d58e
Tom Gray <tom.c.gray@gmail.com>
2020-04-27 11:49:23 +0100

    sandbox hot jar code

--- src/index.jsx
+++ src/index.jsx
@@ -56,6 +56,26 @@ ReactDOM.render(
           })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
     `}</script>
         )}
+
+        {/* if we're on sandbox, load in the sandbox Hotjar code */}
+        {window.location.href.includes('qa.pure-escapes') && (
+          <script type="text/javascript">{`
+          (function(h, o, t, j, a, r) {
+            h.hj =
+              h.hj ||
+              function() {
+                (h.hj.q = h.hj.q || []).push(arguments);
+              };
+            h._hjSettings = { hjid: 1784172, hjsv: 6 };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+          
+    `}</script>
+        )}
       </Helmet>
       <GlobalFonts />
       <GlobalStyle />
