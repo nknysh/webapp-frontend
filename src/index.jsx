18f522e9e4c3c857a4fed36eea882c2389d6c4ee
Tom Gray <tom.c.gray@gmail.com>
2020-04-27 11:41:04 +0100

    if we're on QA, load in the QA hotjar code

--- src/index.jsx
+++ src/index.jsx
@@ -37,6 +37,25 @@ ReactDOM.render(
       <Helmet>
         {headerMeta}
         {headerLink}
+
+        {/* if we're on QA, load in the QA Hotjar code */}
+        {window.location.href.includes('qa.pure-escapes') && (
+          <script type="text/javascript">{`
+          (function(h, o, t, j, a, r) {
+            h.hj =
+              h.hj ||
+              function() {
+                (h.hj.q = h.hj.q || []).push(arguments);
+              };
+            h._hjSettings = { hjid: 1785849, hjsv: 6 };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    `}</script>
+        )}
       </Helmet>
       <GlobalFonts />
       <GlobalStyle />
