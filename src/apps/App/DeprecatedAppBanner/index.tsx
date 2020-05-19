1b421fe99b4be4893954d6fbb497c621d6d031a8
Yarema Sirskyy <yarema@pure-escapes.com>
2020-05-19 19:01:52 +0300

    added banner sceleton

---
+++ src/apps/App/DeprecatedAppBanner/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DABaner } from './DeprecatedAppBanner.styles';
+
+const DeprecatedAppBanner = () =>  (
+  <DABaner>
+    This version of app is deprecated. In order to avoid getting errors, please, reload page to get the latest version.
+  </DABaner>
+);
+
+export default DeprecatedAppBanner;
