119b41e27aa2aabb8304c9ef7803e295af3841f6
andrew wilde <andrew.wilde@jsgeeks.com>
2020-05-25 16:44:04 +0200

    AuthRoute tests & usage for importers

--- src/containers/AuthRoute/index.test.tsx
+++ src/containers/AuthRoute/index.test.tsx
@@ -1,4 +1,7 @@
-import { shouldRedirect } from './';
+import React, { Component } from 'react';
+import { render } from 'enzyme';
+import { MemoryRouter } from "react-router";
+import { AuthRoute, shouldRedirect } from './';
 import { EUserType } from 'services/BackendApi';
 
 describe('AuthRoute', () => {
@@ -14,6 +17,45 @@ describe('AuthRoute', () => {
     expect(shouldRedirect({ isAuthenticated: true, role: EUserType.TA, deny: [EUserType.TA]})).toBe(true);
     expect(shouldRedirect({ isAuthenticated: true, role: EUserType.TA, deny: [EUserType.SR]})).toBe(false);
 
+  });
+
+  it('renders proper component', () => {
+    const Content = () => <div className="content"/>;
+
+    let wrapper = render(
+      <MemoryRouter initialEntries={['/test']}>
+        <AuthRoute path="/test" isAuthenticated={true} component={Content} />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.prop("class")).toEqual("content");
+
+    wrapper = render(
+      <MemoryRouter initialEntries={['/test']}>
+        <AuthRoute path="/test" isAuthenticated={false} component={Content} />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.prop('class')).toBeUndefined();
+
+    wrapper = render(
+      <MemoryRouter initialEntries={['/test']}>
+        <AuthRoute path="/test" isAuthenticated={true} render={props => <Content />} />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.prop("class")).toEqual("content");
+
+    wrapper = render(
+      <MemoryRouter initialEntries={['/test']}>
+        <AuthRoute path="/test" isAuthenticated={true}>
+          <Content />
+        </AuthRoute>
+      </MemoryRouter>
+    );
+
+    expect(wrapper.prop("class")).toEqual("content");
+    
   });
 
 });
